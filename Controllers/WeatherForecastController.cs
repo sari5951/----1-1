525271dcd5d533d1387a74945ec03285c3f94f4c
chaniShmuel <chani@leader.codes>
2024-03-07 20:46:54 +0200

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace שיעו1_1.Controllers{
+//   [ApiController]
+//     [Route("[controller]")]
+//     public class WeatherForecastController : ControllerBase
+//     {
+//         private static readonly string[] Summaries = new[]
+//         {
+//             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//         };
+
+//         private readonly ILogger<WeatherForecastController> _logger;
+
+//         public WeatherForecastController(ILogger<WeatherForecastController> logger)
+//         {
+//             _logger = logger;
+//         }
+
+//         [HttpGet]
+//         public IEnumerable<WeatherForecast> Get()
+//         {
+//             var rng = new Random();
+//             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+//             {
+//                 Date = DateTime.Now.AddDays(index),
+//                 TemperatureC = rng.Next(-20, 55),
+//                 Summary = Summaries[rng.Next(Summaries.Length)]
+//             })
+//             .ToArray();
+//         }
+//     }
+// }
+
+
+    [ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    private static WeatherForecast[] arr;
+    static WeatherForecastController()
+    {var rng = new Random();
+        arr = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+             Date = DateTime.Now.AddDays(index),
+               TemperatureC = rng.Next(-20, 55),
+               Summary = Summaries[rng.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+        
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return arr;
+    }
+
+      [HttpGet("{id}")]
+    public WeatherForecast Get(int id)
+    {
+        return arr[id];
+    }  
+}
+}
